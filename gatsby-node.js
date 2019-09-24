9c117f4978e912f4a73739ea32496e40929593a5
Shiny <contact@shinychang.net>
2019-09-24 20:54:41 +0800

    Blog list pagnation

--- gatsby-node.js
+++ gatsby-node.js
@@ -48,6 +48,21 @@ exports.createPages = ({ graphql, actions }) => {
       })
     })
 
+    const postsPerPage = 6
+    const numPages = Math.ceil(posts.length / postsPerPage)
+    Array.from({ length: numPages }).forEach((_, i) => {
+      createPage({
+        path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+        component: path.resolve("./src/templates/blog-list-template.js"),
+        context: {
+          limit: postsPerPage,
+          skip: i * postsPerPage,
+          numPages,
+          currentPage: i + 1,
+        },
+      })
+    })
+
     return null
   })
 }
---
+++ src/templates/blog-list-template.js
@@ -0,0 +1,134 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+import Button from "../components/button"
+
+class BlogListTemplate extends React.Component {
+  render() {
+    const { data, pageContext } = this.props
+    const { currentPage, numPages } = pageContext
+    const isFirst = currentPage === 1
+    const isLast = currentPage === numPages
+    const prevPage = currentPage - 1 === 1 ? "/" : (currentPage - 1).toString()
+    const nextPage = (currentPage + 1).toString()
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMarkdownRemark.edges
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="All posts" />
+        <Bio />
+        <div style={{ margin: "20px 0 40px" }}>
+          {posts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug
+            return (
+              <div key={node.fields.slug}>
+                <h3
+                  style={{
+                    marginBottom: rhythm(1 / 4),
+                  }}
+                >
+                  <Link
+                    style={{ boxShadow: `none` }}
+                    to={`blog${node.fields.slug}`}
+                  >
+                    {title}
+                  </Link>
+                </h3>
+                <small>{node.frontmatter.date}</small>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                />
+              </div>
+            )
+          })}
+        </div>
+        <ul
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+            alignItems: "center",
+            listStyle: "none",
+            padding: 0,
+          }}
+        >
+          {!isFirst && (
+            <Link to={`/blog/${prevPage}`} rel="prev">
+              ←
+            </Link>
+          )}
+          <li
+            style={{
+              display: "flex",
+              flexGrow: 1,
+              margin: 0,
+              justifyContent: "center",
+            }}
+          >
+            {Array.from({ length: numPages }, (_, i) => (
+              <Link
+                to={`/blog/${i === 0 ? "" : i + 1}`}
+                style={{
+                  padding: rhythm(1 / 4),
+                  marginLeft: rhythm(1 / 4),
+                  marginRight: rhythm(1 / 4),
+                  textDecoration: "none",
+                  color: i + 1 === currentPage ? "#ffffff" : "",
+                  background: i + 1 === currentPage ? "#007acc" : "",
+                }}
+              >
+                {i + 1}
+              </Link>
+            ))}
+          </li>
+          {!isLast && (
+            <Link to={`/blog/${nextPage}`} rel="next">
+              →
+            </Link>
+          )}
+        </ul>
+        <Link to="/">
+          <Button marginTop="85px">Go Home</Button>
+        </Link>
+      </Layout>
+    )
+  }
+}
+
+export default BlogListTemplate
+
+export const pageQuery = graphql`
+  query blogListQuery($skip: Int!, $limit: Int!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
