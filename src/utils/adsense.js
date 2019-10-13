ccadc41cfe5d4a37b8184b30c1f62bd55e812011
Shiny <contact@shinychang.net>
2019-10-13 16:15:40 +0800

    Update adsense.js

--- src/utils/adsense.js
+++ src/utils/adsense.js
@@ -1,4 +1,4 @@
-const adSensePlaceholder = `<ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-1721860710174593" data-ad-slot="4881972037"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>`
+const adSensePlaceholder = `<p><ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-1721860710174593" data-ad-slot="4881972037"></ins></p><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>`
 
 export const injectContentAd = content => {
   let count = 0
