7ded39b7e24c287a8cf11714415535c49c78f39e
Shiny <contact@shinychang.net>
2020-04-09 16:43:43 +0800

    Fix typo

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 
-import Button from "./Button"
+import Button from "./button"
 
 const Share = ({ title, url }) => {
   const [isSupportShare, setIsSupportShare] = useState(true)
